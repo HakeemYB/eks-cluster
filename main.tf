75579b4055d97d801e51720b730f9f9338717b61
HakeemYB <120651914+HakeemYB@users.noreply.github.com>
2023-03-13 22:13:03 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-
-module "eks" {
-   source = "https://github.com/HakeemYB/eks-modules.git"
- 
-  
+provider"aws"{
+  region = "us-east-2"
+}
+module "my_module" {
+source = "git::https://github.com/HakeemYB/eks-modules.git"
 }
