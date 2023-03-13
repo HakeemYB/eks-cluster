b100cb17b14e9bd1194c4065bc0b2a692536c2be
HakeemYB <baighakeemyar.cloudlem@gmail.com>
2023-03-13 18:51:41 +0500

    updated

--- main.tf
+++ main.tf
@@ -1,7 +1,19 @@
 # provider "aws" {
-#   region = "us-east-1"
+#   region = "${var.region}"
 # }
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+
 module "eks" {
-    source = "git@github.com:HakeemYB/eks-terraform-modules.git"
+   source = "git@github.com:HakeemYB/eks-modules.git"
+ 
+  
 }
