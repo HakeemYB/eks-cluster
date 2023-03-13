15ba9cf6c3ffc47f46efcf5a916a7949b58bf83e
HakeemYB <120651914+HakeemYB@users.noreply.github.com>
2023-03-13 20:08:33 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
 terraform {
-  required_version = "0.12.19"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "2.0.0"
+      version = ">= 2.0.0, < 4.0.0"
     }
   }
 }
